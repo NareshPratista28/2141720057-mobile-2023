3d7ebd8f783332f9d0f093920337a61301b39e3a
NareshPratista28 <2141720057@student.polinema.ac.id>
2023-09-18 16:36:58 +0700

    - tugas

--- Week-04/README.md
+++ Week-04/README.md
@@ -138,3 +138,128 @@ Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jela
 > Kode disini menggunakan operator spread. Pada variabel list2, disitu terdapat operator spread ('...') yang berfungsi untuk menggabungkan elemen dari variabel list ke variabel list2 
 
 ### Langkah 3:
+Tambahkan kode program berikut, lalu coba eksekusi (Run) kode Anda.
+```dart
+list1 = [1, 2, null];
+print(list1);
+var list3 = [0, ...?list1];
+print(list3.length);
+```
+![Screenshot](/Week-04/docs/ss_p4_langkah3.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah3_1.PNG)
+> kode di atas menggabungkan elemen dari list1 dan list3 menggunakan operator spread
+
+Tambahkan variabel list berisi NIM Anda menggunakan Spread Operators
+
+![Screenshot](/Week-04/docs/ss_p4_langkah3_2.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah3_3.PNG)
+> disini saya membuat variabel 'nim' yang berisi List dengan elemen nim saya, kemudian menambahkannya kedalam list variabel 'list3' menggunakan spread operator. Jadi, 'list3' akan berisi penggabungan dari elemen yang ada pada 'list1' dan dari 'nim'.
+
+### Langkah 4:
+Tambahkan kode program berikut, lalu coba eksekusi (Run) kode Anda.
+```dart
+var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+print(nav);
+```
+Tunjukkan hasilnya jika variabel promoActive ketika true dan false.
+
+- variabel promoActive = true
+
+![Screenshot](/Week-04/docs/ss_p4_langkah4.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah4_1.PNG)
+> disini saya mendefinisikan variabel 'promoActive' menjadi true. Apabila nilai true, maka elemen 'Outlet' dapat dimasukkan ke dalam list 'nav'.
+
+- variabel promoActive = false
+
+![Screenshot](/Week-04/docs/ss_p4_langkah4_2.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah4_3.PNG)
+> kebalikan dari true, disini saya mendefinisikan variabelnya menjadi false, sehingga elemen 'Outlet' tidak dapat untuk dimasukkan ke dalam list 'nav'.
+
+### Langkah 5:
+Tambahkan kode program berikut, lalu coba eksekusi kode Anda.
+```dart
+var nav2 = ['Home', 'Furniture', 'Plants', if (login case 'Manager') 'Inventory'];
+print(nav2);
+```
+Tunjukkan hasilnya jika variabel login mempunyai kondisi lain.
+
+- kondisi login sebagai user
+![Screenshot](/Week-04/docs/ss_p4_langkah5.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah5_1.PNG)
+
+- kondisi login sebagai Manager
+![Screenshot](/Week-04/docs/ss_p4_langkah5_2.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah5_3.PNG)
+
+> jadi disini saya menggunakan perulangan if-else. jadi variabel 'login' disini akan menentukan apakah elemen 'Inventory' masuk ke dalam list atau tidak, jika nilai dari variabel 'login' adalah Manager, maka elemen 'Inventory' akan masuk ke dalam List 'nav2', sebaliknya apabila nilainya selain 'Manager' maka elemen 'Inventory' tidak akan dimasukkan ke dalam List 'nav2'
+
+### Langkah 6:
+Tambahkan kode program berikut, lalu coba eksekusi (Run) kode Anda.
+```dart
+var listOfInts = [1, 2, 3];
+var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+assert(listOfStrings[1] == '#1');
+print(listOfStrings);
+```
+![Screenshot](/Week-04/docs/ss_p4_langkah6.PNG)
+![Screenshot](/Week-04/docs/ss_p4_langkah6_1.PNG)
+> Kode tersebut menggunakan for-in dalam list literal, jadi elemen yang berada di dalam list tersebut berasal dari setiap elemen 'i' dalam 'listOfInts'
+
+Jelaskan manfaat Collection For
+> Manfaatnya adalah untuk menyederhanakan ataupun memperjelas proses pembuatan List baru berdasarkan elemen dari List yang sudah ada.
+
+## Praktikum 5: Eksperimen Tipe Data Records
+
+### Langkah 1:
+Ketik atau salin kode program berikut ke dalam fungsi main().
+```dart
+var record = ('first', a: 2, b: true, 'last');
+print(record)
+```
+
+### Langkah 2:
+Silahkan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+
+![Screenshot](/Week-04/docs/ss_p5_langkah2.PNG)
+![Screenshot](/Week-04/docs/ss_p5_langkah2_1.PNG)
+
+### Langkah 3:
+Tambahkan kode program berikut di luar scope void main(), lalu coba eksekusi (Run) kode Anda.
+```dart
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
+```
+
+Gunakan fungsi tukar() di dalam main() sehingga tampak jelas proses pertukaran value field di dalam Records.
+
+![Screenshot](/Week-04/docs/ss_p5_langkah3.PNG)
+![Screenshot](/Week-04/docs/ss_p5_langkah3_1.PNG)
+
+### Langkah 4:
+Tambahkan kode program berikut di dalam scope void main(), lalu coba eksekusi (Run) kode Anda.
+```dart
+// Record type annotation in a variable declaration:
+(String, int) mahasiswa;
+print(mahasiswa);
+```
+Inisialisasi field nama dan NIM Anda pada variabel record mahasiswa di atas
+
+![Screenshot](/Week-04/docs/ss_p5_langkah4.PNG)
+![Screenshot](/Week-04/docs/ss_p5_langkah4_1.PNG)
+
+### Langkah 5:
+Tambahkan kode program berikut di dalam scope void main(), lalu coba eksekusi (Run) kode Anda.
+```dart
+var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+print(mahasiswa2.$1); // Prints 'first'
+print(mahasiswa2.a); // Prints 2
+print(mahasiswa2.b); // Prints true
+print(mahasiswa2.$2); // Prints 'last'
+```
+Gantilah salah satu isi record dengan nama dan NIM Anda.
+
+![Screenshot](/Week-04/docs/ss_p5_langkah5.PNG)
+![Screenshot](/Week-04/docs/ss_p5_langkah5_1.PNG)
--- Week-04/src/main.dart
+++ Week-04/src/main.dart
@@ -1,113 +1,160 @@
-void main() {
-  // final List<String?> data = List.filled(5, null);
-  // data[1] = 'Naresh Pratista';
-  // data[2] = '2141720057';
+// void main() {
+// final List<String?> data = List.filled(5, null);
+// data[1] = 'Naresh Pratista';
+// data[2] = '2141720057';
 
-  // String hasil = '';
-  // for (int i = 0; i < data.length; i++) {
-  //   hasil += 'Index ke-$i: ${data[i]}\n';
-  // }
-  // print(hasil);
+// String hasil = '';
+// for (int i = 0; i < data.length; i++) {
+//   hasil += 'Index ke-$i: ${data[i]}\n';
+// }
+// print(hasil);
 
 // Praktikum 2
-  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  // print(halogens);
+// var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+// print(halogens);
 
-  // var names1 = <String>{};
-  // Set<String> names2 = {};
+// var names1 = <String>{};
+// Set<String> names2 = {};
 
-  // names1.add('Naresh Pratista');
-  // names1.add('2141720057');
-  // names2.addAll(['Naresh Pratista, 2141720057']);
+// names1.add('Naresh Pratista');
+// names1.add('2141720057');
+// names2.addAll(['Naresh Pratista, 2141720057']);
 
-  // print(names1);
-  // print(names2);
+// print(names1);
+// print(names2);
 
 // Praktikum 3
-  // var gifts = {
-  //   // Key:    Value
-  //   'first': 'partridge',
-  //   'second': 'turtledoves',
-  //   'fifth': 1
-  // };
+// var gifts = {
+//   // Key:    Value
+//   'first': 'partridge',
+//   'second': 'turtledoves',
+//   'fifth': 1
+// };
 
-  // var nobleGases = {
-  //   2: 'helium',
-  //   10: 'neon',
-  //   18: 2,
-  // };
+// var nobleGases = {
+//   2: 'helium',
+//   10: 'neon',
+//   18: 2,
+// };
 
-  // var mhs1 = Map<String, String>();
-  // gifts['first'] = 'partridge';
-  // gifts['second'] = 'turtledoves';
-  // gifts['fifth'] = 'golden rings';
+// var mhs1 = Map<String, String>();
+// gifts['first'] = 'partridge';
+// gifts['second'] = 'turtledoves';
+// gifts['fifth'] = 'golden rings';
 
-  // var mhs2 = Map<int, String>();
-  // nobleGases[2] = 'helium';
-  // nobleGases[10] = 'neon';
-  // nobleGases[18] = 'argon';
+// var mhs2 = Map<int, String>();
+// nobleGases[2] = 'helium';
+// nobleGases[10] = 'neon';
+// nobleGases[18] = 'argon';
 
-  // print(mhs1);
-  // print(mhs2);
+// print(mhs1);
+// print(mhs2);
 
-  // var mhs1 = Map<String, String>();
-  // mhs1['first'] = 'partridge';
-  // mhs1['second'] = 'turtledoves';
-  // mhs1['fifth'] = 'golden rings';
+// var mhs1 = Map<String, String>();
+// mhs1['first'] = 'partridge';
+// mhs1['second'] = 'turtledoves';
+// mhs1['fifth'] = 'golden rings';
 
-  // var mhs2 = Map<int, String>();
-  // mhs2[2] = 'helium';
-  // mhs2[10] = 'neon';
-  // mhs2[18] = 'argon';
+// var mhs2 = Map<int, String>();
+// mhs2[2] = 'helium';
+// mhs2[10] = 'neon';
+// mhs2[18] = 'argon';
 
-  // print(mhs1);
-  // print(mhs2);
+// print(mhs1);
+// print(mhs2);
 
-  // var gifts = Map<String, String>();
-  // gifts['first'] = 'partridge';
-  // gifts['second'] = 'turtledoves';
-  // gifts['fifth'] = 'golden rings';
-  // gifts['nama'] = 'Naresh Pratista';
-  // gifts['nim'] = '2141720057';
+// var gifts = Map<String, String>();
+// gifts['first'] = 'partridge';
+// gifts['second'] = 'turtledoves';
+// gifts['fifth'] = 'golden rings';
+// gifts['nama'] = 'Naresh Pratista';
+// gifts['nim'] = '2141720057';
 
-  // var nobleGases = Map<int, String>();
-  // nobleGases[1] = 'helium';
-  // nobleGases[2] = 'neon';
-  // nobleGases[3] = 'argon';
-  // nobleGases[4] = 'Naresh Pratista';
-  // nobleGases[5] = '2141720057';
+// var nobleGases = Map<int, String>();
+// nobleGases[1] = 'helium';
+// nobleGases[2] = 'neon';
+// nobleGases[3] = 'argon';
+// nobleGases[4] = 'Naresh Pratista';
+// nobleGases[5] = '2141720057';
 
-  // var mhs1 = Map<String, String>();
-  // mhs1['first'] = 'partridge';
-  // mhs1['second'] = 'turtledoves';
-  // mhs1['fifth'] = 'golden rings';
-  // mhs1['nama'] = 'Naresh Pratista';
-  // mhs1['nim'] = '2141720057';
+// var mhs1 = Map<String, String>();
+// mhs1['first'] = 'partridge';
+// mhs1['second'] = 'turtledoves';
+// mhs1['fifth'] = 'golden rings';
+// mhs1['nama'] = 'Naresh Pratista';
+// mhs1['nim'] = '2141720057';
 
-  // var mhs2 = Map<int, String>();
-  // mhs2[1] = 'helium';
-  // mhs2[2] = 'neon';
-  // mhs2[3] = 'argon';
-  // mhs2[4] = 'Naresh Pratista';
-  // mhs2[5] = '2141720057';
+// var mhs2 = Map<int, String>();
+// mhs2[1] = 'helium';
+// mhs2[2] = 'neon';
+// mhs2[3] = 'argon';
+// mhs2[4] = 'Naresh Pratista';
+// mhs2[5] = '2141720057';
 
-  // print('Map gifts');
-  // print(gifts);
+// print('Map gifts');
+// print(gifts);
 
-  // print('\nMap nobleGases');
-  // print(nobleGases);
+// print('\nMap nobleGases');
+// print(nobleGases);
 
-  // print('\nMap mhs1');
-  // print(mhs1);
+// print('\nMap mhs1');
+// print(mhs1);
 
-  // print('\nMap mhs2');
-  // print(mhs2);
+// print('\nMap mhs2');
+// print(mhs2);
 
 // Praktikum 4
 
-  var list = [1, 2, 3];
-  var list2 = [0, ...list];
-  print(list);
-  print(list2);
-  print(list2.length);
+// var list = [1, 2, 3];
+// var list2 = [0, ...list];
+// print(list);
+// print(list2);
+// print(list2.length);
+
+// var list1 = [1, 2, null];
+// print(list1);
+// var list3 = [0, ...?list1];
+// print(list3);
+// print(list3.length);
+
+// var list1 = [1, 2, null];
+// var nim = ['2141720057'];
+// var list3 = [0, ...?list1, ...nim];
+
+// print(list1);
+// print(list3);
+// print(list3.length);
+
+// var promoActive = true;
+// var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+// print(nav);
+
+// var login = 'Manager';
+// var nav2 = ['Home', 'Furniture', 'Plants'];
+
+// if (login == 'Manager') {
+//   nav2.add('Inventory');
+// }
+
+// print(nav2);
+
+// var listOfInts = [1, 2, 3];
+// var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+// assert(listOfStrings[1] == '#1');
+// print(listOfStrings);
+
+// Praktikum 5
+
+// (int, int) tukar((int, int) record) {
+//   var (a, b) = record;
+//   return (b, a);
+// }
+
+void main() {
+  var mahasiswa2 = ('Naresh Pratista', a: 2141720057, b: true, 'last');
+
+  print(mahasiswa2.$1);
+  print(mahasiswa2.a);
+  print(mahasiswa2.b);
+  print(mahasiswa2.$2);
 }
